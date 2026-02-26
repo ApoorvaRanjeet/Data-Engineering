a2201526e7b18adac68c435a8958cb36611711be
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2026-02-27 00:43:24 +0530

    Add files via upload

---
+++ Python/Integer_floats.py
@@ -0,0 +1,26 @@
+num=3
+
+print(type(num))
+
+n=3.4
+print(type(n))
+print(3/2)
+print(3//2)  # drops the decimal
+print(3**2)  #prints 3 square
+print(3%2)
+
+print(abs(-3))
+print(round(3.75))
+print(round(3.46,1))
+
+a=1
+b=2
+print(a==b)
+print(a!=b)
+print(a>b)
+
+c='1'
+d='2'
+c=int(c) #casting
+d=int(d)
+print(c+d)
