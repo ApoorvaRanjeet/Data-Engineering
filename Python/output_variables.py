fc1713a6e03e61d1ca252eab9ab7e7cbc4f8bb74
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2026-03-06 09:13:22 +0530

    Add files via upload

---
+++ Python/output_variables.py
@@ -0,0 +1,13 @@
+x="a"
+y="b"
+z="c"
+print(x,y,z) # space will be there between charavters
+
+print('*******************************************')
+
+print(x+y+z) # space will not be there between charavters 
+
+print('*******************************************')
+
+t=10
+print(x,t) # to concatenate string and inteer we can use + operator python will throw an error so the best way to do it is to use comma
