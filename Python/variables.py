850a454aedcd7ab709b9a2de9359bcf5b4ad46fc
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2026-03-06 09:13:01 +0530

    Add files via upload

---
+++ Python/variables.py
@@ -0,0 +1,55 @@
+x=5.5
+print(int(x))
+
+print(type(x))
+
+print('*******************************************')
+# camel case 
+myVariable="apoorva"
+
+print(myVariable)
+
+# Pascal case 
+MyVariable="aadrika"
+
+print(MyVariable)
+
+#snake case 
+my_variable_="anusha"
+
+print(my_variable_)
+
+print('*******************************************')
+# assign variables 
+
+x,y,z="apple","banana","orange"
+print(x)
+print(y)
+print(z)
+
+print('*******************************************')
+
+a=b=c="axis"
+print(a)
+print(b)
+print(c)
+
+print('*******************************************')
+
+fruits=["apple","banana","orange"]
+e,f,g=fruits
+print(e)
+print(f)
+print(g)
+
+print('*******************************************')
+
+a=20
+A=30 # case sensitive A willnot overwrite a
+print(a)
+
+print('*******************************************')
+
+q=20
+q="a"
+print(q)
