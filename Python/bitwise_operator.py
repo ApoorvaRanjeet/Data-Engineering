3370d115a9041ca6e2912b7068ba9600651e716b
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2026-03-09 22:47:36 +0530

    Add files via upload

---
+++ Python/bitwise_operator.py
@@ -0,0 +1,10 @@
+x=10
+y=20
+print(x&y)
+print(x|y)
+print(x^y)
+print(~x)
+print(x<<2)
+print(x>>2)
+
+# ^ operator compares each bit and set it to 1 if ony one is 1 oterhwise if both are 1 or 0 inthat case is set to 0.
