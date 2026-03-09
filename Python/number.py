2a0e399643504cab8c28b7d8fae89e49cb0e14be
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2026-03-09 15:03:12 +0530

    Add files via upload

---
+++ Python/number.py
@@ -0,0 +1,26 @@
+# 3 numeric data type in python : 1. int 2. float 3. complex
+x=1
+y=1.2
+z=1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#int is a negative positive or whole number without decimals 
+# flot negative positive number having 1 or more decimals
+#coplex number are written with j
+
+# type conversion
+
+a=float(x)
+print(a)
+
+b=int(y)
+print(b)
+
+c=complex(a)
+print(c)
+
+import random
+print(random.randrange(1,10))
