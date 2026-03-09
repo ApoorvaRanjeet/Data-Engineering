4d1bc0bf87159a97b0f86f2d7380ca492a612b50
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2026-03-09 22:12:06 +0530

    Add files via upload

---
+++ Python/Arithematic_operator.py
@@ -0,0 +1,12 @@
+x = 12
+y = 5
+print(x / y) # return float
+print(x // y) # return integer
+#return the nearest value 
+
+
+#WALRUS OPERATOR 
+list=[12,3,4,4]
+if(count:=len(list))>3:
+    print(f"length of list is {count}")
+    
