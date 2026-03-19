963608b01c1756d5f8690f80ea8843870b06f135
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2026-03-19 10:28:49 +0530

    Add files via upload

---
+++ Python/OOPS/objects.py
@@ -0,0 +1,16 @@
+# creation of class 
+class Myclass:
+    x=5
+    
+# the object of the class
+o1=Myclass()
+print(o1.x)
+
+# to delete objects
+
+del o1
+
+# you want to create the class with no content then you have to use pass statement coz class definitions cannot be empty
+class Myworld:
+    pass
+p1=Myworld()
